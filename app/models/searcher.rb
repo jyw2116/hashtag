1502d4f34985626e2471e34b84bcbb6bff956ea8
Jiyeon Woo <jiyeon8497@gmail.com>
2013-05-21 13:36:23 -0400

    clean up views, routes, specs

--- app/models/searcher.rb
+++ app/models/searcher.rb
@@ -1,9 +1,16 @@
 class Searcher
+	class_attribute :backend
+	self.backend = Twitter
+
 	def initialize(term)
 		@term = term
 	end
 
 	def each(&block)
-		@tweets = Twitter.search(@term).results.each(&block)
+		# class attributes allow setting attributes on a class level and on an instance level (below):
+		backend.search(@term).results.each(&block)
 	end
 end
+
+# this allows for something like :
+# Searcher.backend = FakeTwitter 
